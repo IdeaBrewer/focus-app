411a2fae1922fd577a3d26cc31e5cc323cae55e4
IdeaBrewer <1034168052@qq.com>
2025-09-11 11:57:47 +0800

    Add missing dependencies to resolve Kotlin compilation errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -39,12 +42,49 @@ android {
 }
 
 dependencies {
+    // Core Android dependencies
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
+    // Activity and Fragment
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    
+    // Lifecycle components
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    
+    // Room database
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    kapt("androidx.room:room-compiler:2.5.2")
+    
+    // Hilt dependency injection
+    implementation("com.google.dagger:hilt-android:2.45")
+    kapt("com.google.dagger:hilt-compiler:2.45")
+    
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    
+    // DataStore preferences
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    
+    // RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    
+    // CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+    
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    
+    // Hilt testing
+    testImplementation("com.google.dagger:hilt-android-testing:2.45")
+    kaptTest("com.google.dagger:hilt-compiler:2.45")
 }
